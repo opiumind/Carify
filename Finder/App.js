3bfd6ba13a4a55c7a41d4a3f525ad61762e43c97
[Phongtran1998] <hoang_phong98@me.com>
2018-04-07 11:05:00 -0400

    Add google map

--- Finder/App.js
+++ Finder/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Router, Scene } from 'react-native-router-flux';
+import Map from './src/Map'
 
 const App = () => {
     return (
     <Router showNavigationBar={false}>
         <Scene key={"root"}>
-            <Scene key={"initial"}/>
+            <Scene  key={"initial"}
+                    component={Map}
+                    initial/>
         </Scene>
     </Router>
     )
--- Finder/android/app/src/main/java/com/finder/MainApplication.java
+++ Finder/android/app/src/main/java/com/finder/MainApplication.java
@@ -3,13 +3,16 @@ package com.finder;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.ivanwu.googleapiavailabilitybridge.ReactNativeGooglePlayServicesPackage;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
+import com.airbnb.android.react.maps.MapsPackage;
 import java.util.Arrays;
 import java.util.List;
+
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -22,7 +25,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new ReactNativeGooglePlayServicesPackage(),
+            new MapsPackage()
       );
     }
 
@@ -41,5 +46,12 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+  }
+  
+  protected List<ReactPackage> getPackages() {
+    return Arrays.<ReactPackage>asList(
+            new MainReactPackage(),
+            new MapsPackage()
+    );
   }
 }
